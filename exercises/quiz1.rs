4effdc400720a399b0289ef1db2598b05d5f6ac7
Gal <galc@wix.com>
2023-03-19 22:18:10 +0200

    finished with quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (numOfApples: i32) -> i32{
+    if numOfApples > 40 {
+        numOfApples
+    } else {
+        numOfApples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
